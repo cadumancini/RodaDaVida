51e26295ad2bfdbe7ec1b3c37c1a01663f424b5f
cadumancini <cadumancini@gmail.com>
2017-05-15 20:01:04 -0300

    Alteração no nome do projeto Shared.

--- RodaDaVida/Class1.cs
+++ RodaDaVida/Class1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace RodaDaVida
+namespace RodaDaVidaShared
 {
     public class Class1
     {
--- RodaDaVida/Properties/AssemblyInfo.cs
+++ RodaDaVida/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RodaDaVida")]
+[assembly: AssemblyTitle("RodaDaVidaShared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RodaDaVida")]
+[assembly: AssemblyProduct("RodaDaVidaShared")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
